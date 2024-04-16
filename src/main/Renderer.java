a02a00b6d834675699fd5349630432600a2d6a37
Anhelina Kulkova <angeltutova@gmail.com>
2024-04-16 21:27:18 +0200

    Added scratch camera and key listener

--- src/main/Renderer.java
+++ src/main/Renderer.java
@@ -1,15 +1,15 @@
 package main;
 
 import global.AbstractRenderer;
-import org.lwjgl.glfw.GLFWCursorPosCallback;
-import org.lwjgl.glfw.GLFWScrollCallback;
-import org.lwjgl.glfw.GLFWWindowSizeCallback;
+import global.GLCamera;
+import org.lwjgl.glfw.*;
 
 import java.util.List;
 import java.util.Optional;
 
 import static global.GluUtils.gluLookAt;
 import static global.GluUtils.gluPerspective;
+import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL15.*;
 
@@ -26,6 +26,14 @@ public class Renderer extends AbstractRenderer {
     private int VBOVertices;
     private int VBOIndices;
 
+    private GLCamera camera;
+    private float trans, deltaTrans = 0;
+    private float px, py, pz;
+    private double ex, ey, ez;
+    private float zenit, azimut;
+
+
+
     public Renderer() {
         super();
 
@@ -39,21 +47,62 @@ public class Renderer extends AbstractRenderer {
             }
         };
 
-        /*used default glfwKeyCallback */
+        glfwKeyCallback = new GLFWKeyCallback() {
+            @Override
+            public void invoke(long window, int key, int scancode, int action, int mods) {
+                if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+                    // We will detect this in our rendering loop
+                    glfwSetWindowShouldClose(window, true);
+                if (action == GLFW_RELEASE) {
+//                    trans = 0;
+//                    deltaTrans = 0;
+                }
+                if (action == GLFW_PRESS) {
+                    switch (key) {
+                        case GLFW_KEY_W:
+                            pz -= trans;
+                            camera.forward(trans);
+                            if (deltaTrans < 0.001f)
+                                deltaTrans = 0.001f;
+                            else
+                                deltaTrans *= 1.02;
+                            break;
+                        case GLFW_KEY_S:
+                            pz += trans;
+                            camera.backward(trans);
+                            if (deltaTrans < 0.001f)
+                                deltaTrans = 0.001f;
+                            else
+                                deltaTrans *= 1.02;
+                            break;
 
-        glfwMouseButtonCallback = null; //glfwMouseButtonCallback do nothing
+                    }
+                }
+            }
+        };
+
+        /*used default glfwKeyCallback */
+        glfwMouseButtonCallback = new GLFWMouseButtonCallback() {
+
+            @Override
+            public void invoke(long window, int button, int action, int mods) {
+
+            }
+
+        };
+
 
         glfwCursorPosCallback = new GLFWCursorPosCallback() {
             @Override
             public void invoke(long window, double x, double y) {
-                System.out.println("glfwCursorPosCallback");
+
             }
         };
 
         glfwScrollCallback = new GLFWScrollCallback() {
             @Override
             public void invoke(long window, double dx, double dy) {
-                //do nothing
+                camera.forward(0.1);
             }
         };
     }
@@ -67,12 +116,11 @@ public class Renderer extends AbstractRenderer {
         Optional<List<Object3D>> result = objParser.getObjectsByMaterial("res/data/obj/sneakers.obj");
 
         result.ifPresent(object3DS -> scene = object3DS);
+        scene.remove(3); // remove some redundant planes from obj
 
         glEnable(GL_DEPTH_TEST);
 
-
-
-
+        camera = new GLCamera();
     }
 
 
@@ -81,6 +129,16 @@ public class Renderer extends AbstractRenderer {
         glViewport(0, 0, width*2, height*2); // *2 only for MacOS
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
 
+        trans += deltaTrans;
+
+        double a_rad = azimut * Math.PI / 180;
+        double z_rad = zenit * Math.PI / 180;
+        ex = Math.sin(a_rad) * Math.cos(z_rad);
+        ey = Math.sin(z_rad);
+        ez = -Math.cos(a_rad) * Math.cos(z_rad);
+        double ux = Math.sin(a_rad) * Math.cos(z_rad + Math.PI / 2);
+        double uy = Math.sin(z_rad + Math.PI / 2);
+        double uz = -Math.cos(a_rad) * Math.cos(z_rad + Math.PI / 2);
 
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
@@ -88,9 +146,11 @@ public class Renderer extends AbstractRenderer {
 
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
-        gluLookAt(0.5, -0.5, 0.5,
-                0, 0, 0,
-                0, 0, 1);
+
+        gluLookAt(px, py, pz, ex + px, ey + py, ez + pz, ux, uy, uz);
+//        gluLookAt(1.5, -0.5, 1.0,
+//                0, 0, 0,
+//                0, 1, 0);
         glPushMatrix();
 
 
